cb8acd52db0d627c230d2dcbb69860e86b84b746
zulus23 <zulus23@gmail.com>
2017-10-23 10:34:40 +0300

    add field to address

--- shared/src/main/java/ru/zhukov/recoverdebt/domain/Address.java
+++ shared/src/main/java/ru/zhukov/recoverdebt/domain/Address.java
@@ -1,4 +1,18 @@
 package ru.zhukov.recoverdebt.domain;
 
+
+
 public class Address {
+    private Long id;
+    private String index;
+    private String country;
+    private String region;
+    private String city;
+    private String district;
+    private String street;
+    private String house;
+    private String apartment;
+    private TypeAddress typeAddress;
+
+
 }
