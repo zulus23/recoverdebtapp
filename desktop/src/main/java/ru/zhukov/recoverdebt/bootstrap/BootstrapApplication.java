be5ee9043a14aecd06aec42460b4d01e55b6ff7e
zulus23 <zulus23@gmail.com>
2017-10-28 09:31:05 +0300

    add desktop

---
+++ desktop/src/main/java/ru/zhukov/recoverdebt/bootstrap/BootstrapApplication.java
@@ -0,0 +1,15 @@
+package ru.zhukov.recoverdebt.bootstrap;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BootstrapApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
